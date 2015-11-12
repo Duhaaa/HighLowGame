511dbaa433cffc6438034b1250e4aa546a8d6bfa
Duhaaa <duhaokur@gmail.com>
2015-11-12 10:30:59 +0100

    My first game in C#

---
+++ HighLowGame/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace HighLowGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(19, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(19, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(116, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(19, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "New Game";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "100",
+            "1000"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(170, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(179, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Made by Duha";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(223, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(273, 211);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "High Low";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ HighLowGame/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace HighLowGame
+{
+    public partial class Form1 : Form
+    {
+        //Random class and global variables
+        Random rnd = new Random();
+        int randomnum;
+        int wGuess;
+        int seconds;
+
+        public Form1()
+        {
+            //Initialize the game
+            InitializeComponent();
+            
+            //Labels text
+            label1.Text = "Guess the number!";
+            label2.Text = "Choose your range.";
+            label3.Text = "Wrong guesses: " + wGuess;
+            label5.Text = "0:" + seconds;
+
+            //Buttons properties
+            button1.Enabled = false;
+            button2.Visible = false;   
+        }
+
+        //Perform the following when the user clicks on button1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //If the random number is the same as user input
+            if (Convert.ToInt32(textBox1.Text) == randomnum)
+            {
+                label1.Text = "Correct!";
+                button1.Visible = false;
+                textBox1.Enabled = false;
+                label1.Visible = false;
+                Thread.Sleep(50);
+                label1.Visible = true;
+
+                button2.Visible = true;
+
+                SoundPlayer applause = new SoundPlayer(@"C:\Users\Duha\Music\applause-8.wav");
+                applause.Play();
+
+                this.BackColor = System.Drawing.Color.Green;
+
+                timer1.Stop();
+            }
+            //Else if the random number is LOWER than the user input
+            else if(Convert.ToInt32(textBox1.Text) > randomnum)
+            {
+                label1.Text = "Lower";
+                label1.Visible = false;
+                Thread.Sleep(50);
+                label1.Visible = true;
+                wGuess++;
+                label3.Text = "Wrong guesses: " + wGuess;
+
+                SoundPlayer low = new SoundPlayer(@"C:\Users\Duha\Music\beep4.wav");
+                low.Play();
+
+                this.BackColor = System.Drawing.Color.Red;
+            }
+            //Else if the random number is HIGHER than the user input
+            else if (Convert.ToInt32(textBox1.Text) < randomnum)
+            {
+                label1.Text = "Higher";
+                label1.Visible = false;
+                Thread.Sleep(50);
+                label1.Visible = true;
+                wGuess++;
+                label3.Text = "Wrong guesses: " + wGuess;
+
+                SoundPlayer high = new SoundPlayer(@"C:\Users\Duha\Music\beep1.wav");
+                high.Play();
+
+                this.BackColor = System.Drawing.Color.Blue;
+            }
+            //If the user enters 420 (Easter egg)
+            if (Convert.ToInt32(textBox1.Text) == 420)
+            {
+                SoundPlayer high = new SoundPlayer(@"C:\Users\Duha\Music\hitsound.wav");
+                high.Play();
+            }
+        }
+
+        //This method makes sure the user can press enter and cannot enter
+        //Anything other than numbers
+        //The user will not be able to enter more than 4 numbers
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox1.MaxLength = 4;
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                button1.PerformClick();
+            }
+        }
+        //When the user clicks on button2, the application will restart
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        //The user can choose the range between 10/100/1000
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //If the user chooses 10, the random number will be between 1 and 10
+            //The user will get 10 seconds to guess the random number
+            if (comboBox1.SelectedItem.ToString() == "10")
+            {
+                randomnum = rnd.Next(1, 10);
+                button1.Enabled = true;
+
+                seconds = 10;
+                timer1.Start();
+                label5.Text = "0:" + seconds;
+            }
+
+            //If the user chooses 100, the random number will be between 1 and 100
+            //The user will get 15 seconds to guess the random number
+            if (comboBox1.SelectedItem.ToString() == "100")
+            {
+                randomnum = rnd.Next(1, 100);
+                button1.Enabled = true;
+
+                seconds = 15;
+                timer1.Start();
+                label5.Text = "0:" + seconds;
+            }
+            //If the user chooses 1000, the random number will be between 1 and 1000
+            // The user will get 20 seconds to guess the random number
+            if (comboBox1.SelectedItem.ToString() == "1000")
+            {
+                randomnum = rnd.Next(1, 1000);
+                button1.Enabled = true;
+
+                seconds = 20;
+                timer1.Start();
+                label5.Text = "0:" + seconds;
+            }
+        }
+
+        //The timer counts down and when it hits 0, a messagebox will show and 
+        //the game will restart.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (seconds > 0)
+            {
+                seconds--;
+                label5.Text = "0:" + seconds;
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                Application.Restart();
+            }
+        }
+    }
+}
